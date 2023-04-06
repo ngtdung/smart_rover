8f89b7192e3124de3ca175e9bc7e469e24b79a03
Dustin Nguyen <dustinnguyen1609@gmail.com>
2023-04-06 11:28:15 +0700

    fix

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -26,7 +26,7 @@ def generate_launch_description():
 
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
-    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
+    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','controller_configuration.yaml')
 
     controller_manager = Node(
         package="controller_manager",
@@ -35,7 +35,7 @@ def generate_launch_description():
                     controller_params_file]
     )
 
-    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+    delayed_controller_manager = TimerAction(period=10.0, actions=[controller_manager])
 
     delayed_controller_manager_spawner = TimerAction(
         period=10.0,

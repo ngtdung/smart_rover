203cb460f86e38149956c64a12b53b925e876aea
ngtdung <dustinnguyen1609@gmail.com>
2023-04-09 03:26:08 +0700

    additional fix for real robot

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -21,12 +21,57 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+    )
+
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+
+    controller_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'controller_configuration.yaml')
+
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
                 )]), launch_arguments={'use_sim_time': 'false'}.items()
     )
 
+    delayed_controller_manager = TimerAction(
+        period=3.0,
+        actions=[
+            Node(
+                package="controller_manager",
+                executable="ros2_control_node",
+                parameters=[{'robot_description': robot_description}, 
+                            controller_params_file], 
+            )])       
+    diff_drive_spawner=Node(
+                package="controller_manager",
+                executable="spawner.py",
+                arguments=["diff_cont"],
+            )
+    delayed_diff_drive_spawner = RegisterEventHandler(
+          event_handler=OnProcessStart(
+          target_action=delayed_controller_manager,
+          on_start=[diff_drive_spawner],
+        )
+        )       
+    joint_broad_spawner= Node(
+                package="controller_manager",
+                executable="spawner.py",
+                arguments=["joint_broad"],
+            )
+    delayed_joint_broad_spawner = RegisterEventHandler(
+          event_handler=OnProcessStart(
+          target_action=delayed_controller_manager,
+          on_start=[joint_broad_spawner],
+        )
+        )     
     # Launch!
     return LaunchDescription(
         [
         rsp,
+        joystick,
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner
         ]
     )

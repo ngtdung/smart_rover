46c2ab0886ecbb6ed5ac3d6c2b36fa89a27ddb66
ngtdung <dustinnguyen1609@gmail.com>
2023-04-15 22:29:33 +0700

    final version

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.actions import IncludeLaunchDescription, GroupAction
 from launch.substitutions import Command
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import RegisterEventHandler
@@ -24,54 +24,22 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
-    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    rf2o_launch_dir = os.path.join(get_package_share_directory('rf2o_laser_odometry'), 'launch')
+    slam_launch_dir = os.path.join(get_package_share_directory('neuronbot2_slam'), 'launch')
 
-    controller_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'controller_configuration.yaml')
+    pure_lidar_slam = GroupAction([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([rf2o_launch_dir, '/rf2o_laser_odometry.launch.py']),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([slam_launch_dir, '/gmapping_launch.py']),
+        ),
+    ])
 
-    joystick = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','joystick.launch.py'
-                )]), launch_arguments={'use_sim_time': 'false'}.items()
-    )
-
-    delayed_controller_manager = TimerAction(
-        period=3.0,
-        actions=[
-            Node(
-                package="controller_manager",
-                executable="ros2_control_node",
-                parameters=[{'robot_description': robot_description}, 
-                            controller_params_file], 
-            )])       
-    diff_drive_spawner=Node(
-                package="controller_manager",
-                executable="spawner.py",
-                arguments=["diff_cont"],
-            )
-    delayed_diff_drive_spawner = RegisterEventHandler(
-          event_handler=OnProcessStart(
-          target_action=delayed_controller_manager,
-          on_start=[diff_drive_spawner],
-        )
-        )       
-    joint_broad_spawner= Node(
-                package="controller_manager",
-                executable="spawner.py",
-                arguments=["joint_broad"],
-            )
-    delayed_joint_broad_spawner = RegisterEventHandler(
-          event_handler=OnProcessStart(
-          target_action=delayed_controller_manager,
-          on_start=[joint_broad_spawner],
-        )
-        )     
     # Launch!
     return LaunchDescription(
         [
         rsp,
-        joystick,
-        delayed_controller_manager,
-        delayed_diff_drive_spawner,
-        delayed_joint_broad_spawner
+        pure_lidar_slam
         ]
     )
